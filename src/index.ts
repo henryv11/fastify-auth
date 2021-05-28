70e7143d1610b4a833f4054360bcf77115ae3930
henry viirmäe <heviir@ttu.ee>
2021-05-28 21:38:27 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,169 @@
+import httpErrors from '@heviir/http-errors';
+import { FastifyInstance, FastifyRequest, preValidationHookHandler } from 'fastify';
+import fp from 'fastify-plugin';
+import * as jwt from 'jsonwebtoken';
+
+const TOKEN_HEADER_KEY = 'authorization';
+const TOKEN_EXAMPLE =
+  'Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0IiwiaWF0IjoxNjAxODE0Mzc4fQ.Cqo8aBPhJN-hVN9wpAYNnIbLZ8M8ORMAMj_6ZIQTGV_g1hx3dti5Qjelgup2eh2dEnbP3aNmLqHKA7vYrJZjBQ';
+const TOKEN_DESCRIPTION = 'JSON Web Token in format "Bearer [token]';
+const ALGORITHM = 'RS512';
+const ISSUER = 'heviir/authorization';
+const JWT_ID = 'jwt_id';
+
+export default fp(
+  async function fastifyAuth(
+    app,
+    {
+      privateKey: maybePrivateKeyPromise,
+      publicKey: maybePublicKeyPromise,
+      permissions: maybePermissionsPromise,
+    }: {
+      /**
+       * @param privateKey to encode token, only needed for auth server
+       */
+      privateKey?: string | Buffer | Promise<string | Buffer>;
+      /**
+       * @param publicKey to decode token
+       */
+      publicKey: string | Buffer | Promise<string | Buffer>;
+      /**
+       * @param permissions from auth server
+       */
+      permissions: Permissions | Promise<Permissions>;
+    },
+  ) {
+    const [permissions, privateKey, publicKey] = await Promise.all([
+      maybePermissionsPromise,
+      maybePrivateKeyPromise,
+      maybePublicKeyPromise,
+    ]);
+    app.decorate(
+      'auth',
+      Object.freeze<FastifyInstance['auth']>({
+        decodeToken(token) {
+          if (!token) return Promise.reject(Error('missing token'));
+          return new Promise<DecodedToken>((resolve, reject) =>
+            jwt.verify(token, publicKey, { algorithms: [ALGORITHM], issuer: [ISSUER], jwtid: JWT_ID }, (err, decoded) =>
+              err || !decoded
+                ? reject(err || new Error('failed to decode token'))
+                : resolve(<DecodedToken>{ ...decoded, roles: new Set((<RawDecodedToken>decoded).roles) }),
+            ),
+          );
+        },
+        encodeToken({ roles, sub }) {
+          if (!privateKey) return Promise.reject(new TypeError('private key is needed to encode token'));
+          return new Promise((resolve, reject) =>
+            jwt.sign(
+              { sub, roles },
+              privateKey,
+              { algorithm: ALGORITHM, issuer: ISSUER, expiresIn: '12h', jwtid: JWT_ID },
+              (err, encoded) =>
+                err || !encoded ? reject(err || new Error('failed to encode token')) : resolve(encoded),
+            ),
+          );
+        },
+      }),
+    );
+    app.decorateRequest('token', null);
+    app.decorateRequest('tokenError', '');
+    app.decorateRequest('getToken', async function (this: FastifyRequest): ReturnType<FastifyRequest['getToken']> {
+      const result = { token: this.token, error: this.tokenError };
+      if (result.token || result.error) return result;
+      const rawToken = this.headers[TOKEN_HEADER_KEY]?.split(' ')[1];
+      try {
+        result.token = this.token = await app.auth.decodeToken(rawToken);
+      } catch (error) {
+        result.error = this.tokenError = error.message;
+      }
+      return result;
+    });
+    app.addHook('onRoute', function (routeOptions) {
+      const options = routeOptions.auth;
+      if (!options) return;
+      let strategy: (req: FastifyRequest, token: NonNullable<FastifyRequest['token']>) => void;
+      switch (typeof options) {
+        case 'string':
+          const [resource, operation] = (<string>options).split(':');
+          if (!permissions[resource]) throw new TypeError('invalid resource ' + resource);
+          const resourcePermissions = permissions[resource][<Operation>operation];
+          if (!resourcePermissions) throw new TypeError('invalid operation ' + operation);
+          strategy = (_, token) => {
+            if (!resourcePermissions.some(role => token.roles.has(role)))
+              throw new httpErrors.Forbidden({ response: 'lacking permission to ' + operation + ' resource' });
+          };
+          break;
+        case 'boolean':
+        case 'object':
+          strategy = () => void 0;
+          break;
+        default:
+          throw new TypeError('invalid authorization config type ' + typeof options);
+      }
+      routeOptions.schema = {
+        ...routeOptions.schema,
+        headers: {
+          /* eslint-disable @typescript-eslint/no-explicit-any */
+          ...(<any>routeOptions.schema?.headers),
+          type: 'object',
+          required: [...((<any>routeOptions.schema?.headers)?.required || [])].concat(TOKEN_HEADER_KEY),
+          properties: {
+            ...(<any>routeOptions.schema?.headers)?.properties,
+            /* eslint-enable @typescript-eslint/no-explicit-any */
+            [TOKEN_HEADER_KEY]: {
+              type: 'string',
+              example: TOKEN_EXAMPLE,
+              description: TOKEN_DESCRIPTION,
+            },
+          },
+        },
+      };
+      routeOptions.preValidation = (<preValidationHookHandler[]>[]).concat(async function (req) {
+        const { error, token } = await req.getToken();
+        if (!token) throw new httpErrors.Unauthorized({ response: error });
+        strategy(req, token);
+      }, routeOptions.preValidation || []);
+    });
+  },
+  { name: 'fastify-auth' },
+);
+
+interface RawDecodedToken {
+  sub: string | number;
+  iat: number;
+  roles?: string[];
+}
+
+interface DecodedToken extends Omit<RawDecodedToken, 'roles'> {
+  roles: Set<string>;
+}
+
+type Operation = 'create' | 'read' | 'update' | 'delete';
+
+interface Permissions {
+  [resource: string]: Partial<Record<Operation, string[]>>;
+}
+
+type AuthConfig = string | boolean;
+
+declare module 'fastify' {
+  interface FastifyInstance {
+    auth: {
+      decodeToken: (token?: string) => Promise<NonNullable<FastifyRequest['token']>>;
+      encodeToken: (payload: { sub: string; roles?: string[] }) => Promise<string>;
+    };
+  }
+
+  interface FastifyRequest {
+    token: DecodedToken | null;
+    tokenError: string | '';
+    getToken: () => Promise<{ token: FastifyRequest['token']; error: FastifyRequest['tokenError'] }>;
+  }
+
+  interface RouteOptions {
+    auth?: AuthConfig;
+  }
+  interface RouteShorthandOptions {
+    auth?: AuthConfig;
+  }
+}
---
+++ test/index.test.ts
@@ -0,0 +1,52 @@
+import fastify from 'fastify';
+import { readFile as readFileFS } from 'fs';
+import { promisify } from 'util';
+import fastifyAuth from '../src';
+
+const readFile = promisify(readFileFS);
+
+function getPermissions() {
+  return Promise.resolve({
+    hello: {
+      read: ['user', 'admin'],
+      create: ['admin'],
+      update: ['admin'],
+    },
+  });
+}
+
+function getApp() {
+  const app = fastify({
+    logger: !true,
+  });
+  app.register(fastifyAuth, {
+    privateKey: readFile(__dirname + '/privateKey.pem'),
+    publicKey: readFile(__dirname + '/publicKey.pem'),
+    permissions: getPermissions(),
+  });
+  return app;
+}
+
+describe('fastify auth', () => {
+  test('it works', async () => {
+    const app = getApp();
+    app.get('/', {
+      auth: 'hello:read',
+      handler: (req, res) => {
+        res.send(req.token);
+      },
+    });
+    await app.ready();
+    const makeReq = async (token?: string | Promise<string>) =>
+      app.inject({ method: 'GET', path: '/', headers: { ...(token && { authorization: 'bearer ' + (await token) }) } });
+
+    let res = await makeReq();
+    expect(res.json().message).toEqual('Unauthorized');
+
+    res = await makeReq(app.auth.encodeToken({ sub: 'blin', roles: ['user'] }));
+    expect(res.json().sub).toEqual('blin');
+
+    res = await makeReq(await app.auth.encodeToken({ sub: 'blin' }));
+    expect(res.json().message).toEqual('Forbidden');
+  });
+});
